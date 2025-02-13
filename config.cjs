5dfee2dfde36ed5043572f308054e0a69f91426d
riyaj223311 <md8652328@gmail.com>
2025-02-13 23:36:57 +0600

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-  "SESSION_ID": process.env.SESSION_ID || "",
+  "SESSION_ID": process.env.SESSION_ID || "levanter_1389f0ee0b8bfb43e9820001906ae698f1",
   "ALIVE_IMG": process.env.ALIVE_IMG || "https://raw.githubusercontent.com/HyHamza/HyHamza/refs/heads/main/Images/logo.jpg",
   "PREFIX": process.env.PREFIX || ".",
   "AUTO_READ_STATUS": process.env.AUTO_READ_STATUS === 'true',
